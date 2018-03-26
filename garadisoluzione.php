c4294023d073968bf806d7f56c57f8aef5a57847
x18349276 <x18349276@users.noreply.github.com>
2018-03-26 19:59:27 +0200

    Prova

--- garadisoluzione.php
+++ garadisoluzione.php
@@ -3,7 +3,7 @@
 $a = $_POST['a'];
 
 // uso la funzione mail di PHP per inviare questi dati al mio indirizzo di posta
-mail('vale18349276@gmail.com', 'Risposta gara di soluzione', $a);
+
 
 // Mostro un messaggio di conferma all'utente
 echo 'Al prossimo evento JCC';
