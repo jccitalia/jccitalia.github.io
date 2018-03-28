3cdee4ed63e125f5b3c45e6c05b136b7b4929ca8
x18349276 <x18349276@users.noreply.github.com>
2018-03-28 21:32:58 +0200

    Prova

--- garadisoluzione.php
+++ garadisoluzione.php
@@ -1,5 +1,13 @@
+<!DOCTYPE html>
+<html>
 <?php
+// Recupero i valori inseriti nel form
+$a = $_POST['a'];
+
+// uso la funzione mail di PHP per inviare questi dati al mio indirizzo di posta
+mail('vale18349276@gmail.com', 'Risposta gara di soluzione', $a);
 
 // Mostro un messaggio di conferma all'utente
 echo 'Al prossimo evento JCC';
 ?>
+</html>
