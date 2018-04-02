f8bbd21a25714e1a1f507e4e89517234f70cc26b
x18349276 <x18349276@users.noreply.github.com>
2018-04-02 16:48:35 +0200

    Prova

--- garadisoluzione.php
+++ garadisoluzione.php
@@ -10,15 +10,6 @@
   <link rel="shortcut icon" href="images/favicon.ico">
 </head>
 <body>
-<?php
-// Recupero i valori inseriti nel form
-$a = $_POST['a'];
 
-// uso la funzione mail di PHP per inviare questi dati al mio indirizzo di posta
-mail('vale18349276@gmail.com', 'Risposta gara di soluzione', $a);
-
-// Mostro un messaggio di conferma all'utente
-echo 'Al prossimo evento JCC';
-?>
 </body>
 </html>
