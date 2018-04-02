387f00c3aecdfb189438f64863d27651c3de1e00
x18349276 <x18349276@users.noreply.github.com>
2018-04-02 16:43:05 +0200

    Prova

--- garadisoluzione.php
+++ garadisoluzione.php
@@ -1,5 +1,15 @@
 <!DOCTYPE html>
 <html>
+<head>
+  <title>1° JCC Italia | L'innovativa coppa U18</title>
+  <meta charset="utf-8"/>
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+  <link href='https://fonts.googleapis.com/css?family=Titillium+Web|Bungee' rel='stylesheet' type='text/css'>
+  <link rel="stylesheet" type="text/css" href="main.css" id="theme">
+  <link rel="shortcut icon" href="images/favicon.ico">
+</head>
+<body>
 <?php
 // Recupero i valori inseriti nel form
 $a = $_POST['a'];
@@ -10,4 +20,5 @@ mail('vale18349276@gmail.com', 'Risposta gara di soluzione', $a);
 // Mostro un messaggio di conferma all'utente
 echo 'Al prossimo evento JCC';
 ?>
+</body>
 </html>
